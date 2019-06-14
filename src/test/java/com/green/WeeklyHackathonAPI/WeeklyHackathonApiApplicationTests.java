e01cbef20afc3ce12f5ff2b8023070f1f5342818
Karthik Rao and Nicole Wright <pair+krao+twright@kr.af.mil>
2019-06-14 09:15:50 -0400

    Initialized SpringBoot

---
+++ src/test/java/com/green/WeeklyHackathonAPI/WeeklyHackathonApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.green.WeeklyHackathonAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeeklyHackathonApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
